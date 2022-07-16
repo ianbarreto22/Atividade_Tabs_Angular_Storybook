591db292f023106d878e3cc21f5050f57b2e0742
Ian Barreto <ian.barreto@hotmail.com>
2022-07-16 19:57:00 -0300

    Correção

--- quiz/src/app/app.module.ts
+++ quiz/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { PanelComponent } from "./components/panel/app.panel.component";
 import { TabComponent } from "./components/tab/app.tab.component";
-import { PanelFormComponent } from "./components/panel-form/panel-form.component";
+import { PanelFormComponent } from "./components/panel-form/app.panel-form.component";
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
